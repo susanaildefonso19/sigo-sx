8ef0eebe14cf890836c55a3060780baeaa605bbb
susanaildefonso19 <susanaildefonso19@gmail.com>
2025-06-26 21:35:32 +0100

    Fix email authentication credentials and improve error handling in sendMail function

--- codigo/backend/services/emailService.js
+++ codigo/backend/services/emailService.js
@@ -5,18 +5,24 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'susanaildefonso19@gmail.com',
-    pass: 'srdi hxej uuuu xgse'
+    pass: 'omdt tcgg sofl nmul'
   }
+  
 }); // Configura o serviço de email e as credenciais de autenticação
 
 async function sendMail({ to, subject, text, html }) {
-  return transporter.sendMail({
-    from: '"SIGO-SX" <susanaildefonso19@gmail.com>',
-    to,
-    subject,
-    text,
-    html
-  });
+  try {
+    return await transporter.sendMail({
+      from: '"SIGO-SX" <susanaildefonso19@gmail.com>',
+      to,
+      subject,
+      text,
+      html
+    });
+  } catch (error) {
+    console.error('Erro ao enviar email:', error);
+    throw error;
+  }
 } // Função para enviar emails, recebe um objeto com os detalhes do email
 
 module.exports = { sendMail }; // Exporta a função sendMail para ser usada em outros módulos
